189cbdc95354bc37e6a39b20356c21765b27ed74
Hez <hezmonaghan@gmail.com>
2017-02-19 18:47:45 +1000

    somewhat complete!
    Authenticating correctly, and rendering data. Videos also playable.
    Works fine on Chrome. Some issues still on Fox and Saf.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { Http, Headers} from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+
+  kwickFeed = [];
+
+  constructor(private http: Http){
+
+    var token = "";
+
+    this.http.post('https://bigdev.kwickie.com/api/Members/login', {
+      email: 'external.testuser@kwickie.com',
+      password: 'KwickieRocks!'
+    }).subscribe((res) => {
+
+      var data = res.json();
+      var arr = [];
+      for (var prop in data) {
+        arr.push(data[prop]);
+      }
+
+      token = arr[0];
+
+      var feedarr = [];
+
+      this.http.get('https://bigdev.kwickie.com/api/Members/me/kwickie/feed?includeQuestions=false&excludeAssociatedKwickies=false&offset=0&limit=25&access_token='+token).subscribe((respo) => {
+        var feeddata = respo.json();
+        for (var feedprop in feeddata) {
+          feedarr.push(feeddata[feedprop]);
+          this.kwickFeed.push(feeddata[feedprop]);
+        }
+        console.log(feedarr);
+
+
+      });
+
+    });
+
+  }
+
 }
